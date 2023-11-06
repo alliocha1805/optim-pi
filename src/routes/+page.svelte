72e80d93659b06d50f69e6312aefcb69f05f1297
alliocha1805 <alexis.piloy@gmail.com>
2023-11-06 16:31:54 +0100

    appel front fonctionnel

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,8 +5,8 @@
 	import { Stepper, Step } from '@skeletonlabs/skeleton';
 	let maxLoadPercent=100;
 	let maxLoad=maxLoadPercent/100;
-	let numberTeam=1;
-	let numFeature=1;
+	let numberTeam=2;
+	let numFeature=2;
 	let resultat = {}
 	let tableData = [];
 	let tableDataClean = [];
@@ -15,36 +15,44 @@
 	let resultReceived = false;
 
 	
-	async function optimize(data){
-		console.log("launching data request")
-		const res = await fetch('/api/optimize', {
+	async function optimize(dataClean) {
+		// Use the environment variables for API URL and API key
+		const apiUrl = "https://test-api-python-vercel.vercel.app/optimize/";
+		const apiKey = "poneychoucroute";
+		const requestData = {
+			"data": {
+			'maxLoad': maxLoad, // Add the maxLoad value here
+			'tasks': dataClean,      // Use the data from the function parameter
+			},
+		};
+		const response = await fetch(apiUrl, {
 			method: 'POST',
 			headers: {
-				'Content-Type': 'application/json',
-				'X-API-Key': 'poneychoucroute',
+			'Content-Type': 'application/json',
+			'X-API-Key': apiKey,
 			},
-			body: JSON.stringify(data),
-			})
-		console.log("waiting data response")
-		console.log(res)
-		const json = await res.json()
-		console.log(json)
-		if(res.ok){
-			resultat = JSON.stringify(json);
-			console.log(json)
-		}
-		else{
-			console.log(json)
-		}
-		
-	}
+			body: JSON.stringify(requestData),
+		});
 
-	function delay(ms: number) {
-			return new Promise( resolve => setTimeout(resolve, ms) );
+		console.log("reponse");
+		console.log(response);
+
+		if (response.ok) {
+			const json = await response.json();
+			console.log(json);
+			//resultat = JSON.stringify(json);
+			resultat = json
+			console.log(resultat);
+			resultReceived = true;
+		} else {
+			const json = await response.json(); // Parse error response
+			console.log(json);
+		}
 	}
 
 	const backStepper = () =>  {
 		visibleResult=false;
+		resultReceived=false;
 	}
 
 	const collectAndCleanCellValues = () => {
@@ -56,8 +64,13 @@
 					rows.forEach(row => {
 							const rowData = [];
 							const cells = row.querySelectorAll('td input');
-							cells.forEach(cell => {
+							cells.forEach((cell, index) => {
+								if (index === 1) {
+									// Convert the second value to a float
+									rowData.push(parseFloat(cell.value));
+								} else {
 									rowData.push(cell.value);
+								}
 							});
 							tableData.push(rowData);
 					});
@@ -66,14 +79,15 @@
 					tableDataClean = tableData.map(row => {
 							return row.map((value, index) => {
 									if (index < 2) {
-											return value; // Don't divide the first two values
-									} else {
+											return value;
+									 // Don't divide the first two values
+									}
+									else {
 											return value / 100; // Divide other values by 100
 									}
 							});
 					});
 					visibleResult=true;
-					console.log(tableDataClean);
 					optimize(tableDataClean);
 			}
 	};
@@ -133,27 +147,63 @@
 		</Step>
 		</Stepper>
 	</div>
-	{#if visibleResult}
-		{resultat}
-		{#if resultReceived}
-		<p>prout</p>
-		{/if}
-		<section class="card w-full mt-5">
-			<div class="p-4 space-y-4">
+	{#if visibleResult}		
+	{#if !resultReceived}
+	<section class="card w-full mt-5">
+		<div class="p-4 space-y-4">
+			<div class="placeholder animate-pulse" />
+			<div class="grid grid-cols-3 gap-8">
 				<div class="placeholder animate-pulse" />
-				<div class="grid grid-cols-3 gap-8">
-					<div class="placeholder animate-pulse" />
-					<div class="placeholder animate-pulse" />
-					<div class="placeholder animate-pulse" />
-				</div>
-				<div class="grid grid-cols-4 gap-4">
-					<div class="placeholder animate-pulse" />
-					<div class="placeholder animate-pulse" />
-					<div class="placeholder animate-pulse" />
-					<div class="placeholder animate-pulse" />
-				</div>
+				<div class="placeholder animate-pulse" />
+				<div class="placeholder animate-pulse" />
 			</div>
-		</section>
+			<div class="grid grid-cols-4 gap-4">
+				<div class="placeholder animate-pulse" />
+				<div class="placeholder animate-pulse" />
+				<div class="placeholder animate-pulse" />
+				<div class="placeholder animate-pulse" />
+			</div>
+		</div>
+	</section>
+	{/if}	
+	{/if}
+	{#if resultReceived}
+		{#if resultat}
+		<div class="card p-4 mt-5">
+			<p> Valeur maximale pouvant être délivrée :{JSON.parse(resultat.ValeurTotale)} </p><br/>
+			<p> Features à prioriser</p>
+			<ul class="list">
+			{#each Object.entries(resultat.Feature) as [nom, statut]}
+			<li>
+				{#if statut == false}
+				<span><svg class="h-8 w-8 text-red-500"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
+					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636"/>
+				  </svg>
+				  </span>
+				{/if}
+				{#if statut == true}
+				<span><svg class="h-8 w-8 text-green-500"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
+					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"/>
+				  </svg>
+				  </span>
+				{/if}
+				<span class="flex-auto">{nom}</span>
+			</li>
+			{/each}
+			</ul><br/>
+			<p> Charge equipe</p>
+			<ul class="list">
+			{#each Object.entries(resultat.ChargeEquipes) as [nom, valeur]}
+			<li>
+				<span>{nom}</span>
+				<span class="flex-auto">{valeur}</span>
+			</li>
+			{/each}
+			</ul>
+		</div>
+		{/if}
+	
+
 	{/if}
 </div>
 
